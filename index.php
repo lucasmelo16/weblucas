9475b6784ff902c834bfb5b191463557db49b31a
Lucas Patrocínio Melo - 11816233 <11816233@animaedu.intranet>
2019-10-04 20:34:06 -0300

    Primeiro Arquivo

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php $numero1 = 1;
+	echo $numero1;
+	echo "<br>Olá Mundo<br>";
+	define("ip","127.0.0.1");
+	echo ip;
+	$numero2 = 2;
+	echo ("<br>O Resultado da soma é:".($numero1 + $numero2));
+	?>
